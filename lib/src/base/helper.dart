7eec16c90594b4f319e5cf48dc384521bd0d2b28
congson99 <congson99vn@gmail.com>
2025-05-06 10:30:28 +0700

    Fix deprecated color usage

--- lib/src/base/helper.dart
+++ lib/src/base/helper.dart
@@ -42,7 +42,7 @@ double reCalculateBorderRadiusByAvatarSize(double? radius, Size? size) {
 
 Color getContrastColorByRootColor(Color color) {
   double luminance =
-      (0.299 * color.red + 0.587 * color.green + 0.114 * color.blue) / 255;
+      (0.299 * color.r + 0.587 * color.g + 0.114 * color.b) / 255;
   if (luminance > 0.5) {
     return Color.lerp(Colors.black, Colors.grey, (luminance - 0.5) * 2)!;
   }
